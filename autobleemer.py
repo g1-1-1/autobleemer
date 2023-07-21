0604f9137cc7c5f0c7a376f21a6d42795f630b12
jupiter <syl@waifu.club>
2023-07-21 21:28:23 +1000

    feat: add debugging capabilities

--- autobleemer.py
+++ autobleemer.py
@@ -7,6 +7,8 @@ from tkinter import messagebox
 # globals
 tools = os.path.join(os.getcwd(), "tools")
 root = tk.Tk()
+root.iconbitmap("assets/icon.ico")
+debug = False
 
 
 def is_admin():
@@ -18,31 +20,54 @@ def is_admin():
 
 
 def update_status(status_text):
-    '''update top-left status box.'''
+    """update top-left status box."""
     global status_label  # use the global status_label
     status_label.config(text=status_text)
     root.update_idletasks()
+    if debug:
+        print(status_text)
 
 
 def run_cdrecord(scsi_id, speed):
-    """this is just a full port of the bash commands using subprocess."""
     cdrecord = os.path.join(tools, "cdrecord")
     mkisofs = os.path.join(tools, "mkisofs")
     ippatch = os.path.join(tools, "ippatch")
     data_iso = os.path.join(tools, "data.iso")
     data_path = os.path.join(tools, "data/")
     ip_bin = os.path.join(tools, "IP.BIN")
+    msinfo = os.path.join(tools, "msinfo.txt")
 
-    with open("msinfo.txt", "w") as f:
+    with open(msinfo, "w") as f:
         subprocess.run([cdrecord, f"-dev={scsi_id}", "-msinfo"], stdout=f)
 
     update_status("creating data.iso with mkisofs...")
+    if debug:
+        print(
+            "mkisofs command:",
+            [
+                mkisofs,
+                "-C",
+                f"@{msinfo}",
+                "-V",
+                "BLEEM!",
+                "-l",
+                "-o",
+                data_iso,
+                data_path,
+            ],
+        )
     mkisofs_process = subprocess.run(
-        [mkisofs, "-C", "@msinfo.txt", "-V", "BLEEM!", "-l", "-o", data_iso, data_path],
+        [mkisofs, "-C", f"@{msinfo}", "-V", "BLEEM!", "-l", "-o", data_iso, data_path],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
     )
+
+    # display stdout and stderr from mkisofs_process
+    if debug:
+        print("mkisofs stdout:", mkisofs_process.stdout)
+        print("mkisofs stderr:", mkisofs_process.stderr)
+
     if any(
         error_phrase in mkisofs_process.stderr
         for error_phrase in ["No such file or directory", "Invalid node", "Malformed"]
@@ -52,16 +77,22 @@ def run_cdrecord(scsi_id, speed):
     else:
         update_status("mkisofs completed.")
 
-    os.remove("msinfo.txt")
+    os.remove(msinfo)
     update_status("msinfo deleted.")
 
-    update_status("patching data.iso with ippatch...")
+    update_status("\npatching data.iso with ippatch...")
     ippatch_process = subprocess.run(
         [ippatch, data_iso, ip_bin],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
     )
+
+    # display stdout and stderr from ippatch_process
+    if debug:
+        print("ippatch stdout:", ippatch_process.stdout)
+        print("ippatch stderr:", ippatch_process.stderr)
+
     if any(
         error_phrase in ippatch_process.stderr
         for error_phrase in ["No such file or directory", "can't open"]
@@ -71,21 +102,29 @@ def run_cdrecord(scsi_id, speed):
     else:
         update_status("ippatch completed.")
 
+    update_status("\nstarting data.iso burn...")
     cdrecord_process = subprocess.run(
-        [cdrecord, f"-dev={scsi_id}", "-xa1", f"speed={speed}", data_iso, "-eject"],
+        [cdrecord, f"-dev={scsi_id}", "-xa1", f"speed={speed}", data_iso],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
     )
-    if any(
-        error_phrase in cdrecord_process.stderr
-        for error_phrase in ["No such file or directory", "can't open"]
+
+    # display stdout and stderr from cdrecord_process
+    if debug:
+        print("cdrecord stdout:", cdrecord_process.stdout)
+        print("cdrecord stderr:", cdrecord_process.stderr)
+
+    if (
+        "No such file or directory" in cdrecord_process.stdout
+        or "can't open" in cdrecord_process.stdout
+        or "No such file or directory" in cdrecord_process.stderr
+        or "can't open" in cdrecord_process.stderr
     ):
         update_status("cdrecord error: " + cdrecord_process.stderr)
         raise RuntimeError("cdrecord error: " + cdrecord_process.stderr)
     else:
         update_status("data.iso burn completed.")
-        update_status("ejected disc from drive.")
 
     os.remove(data_iso)
     update_status("data.iso deleted.")
@@ -125,6 +164,12 @@ def main():
     status_label = tk.Label(root, text="initialising", padx=10, pady=10)
     status_label.pack()
 
+    # tell the user to wait for everything to finish
+    messagebox.showinfo(
+        "starting to autobleem...",
+        "please wait for everything to finish! do not close any windows without a prompt.",
+    )
+
     # get the first CD drive available
     scsi_id = get_first_cd_drive()
     if scsi_id is None:
@@ -133,12 +178,6 @@ def main():
         return
 
     speed = "10"  # refactor out of existence
-
-    # tell the user to wait for everything to finish
-    messagebox.showinfo(
-        "starting to autobleem...",
-        "please wait for everything to finish! do not close any windows without a prompt.",
-    )
 
     try:
         # run the CD recording process
@@ -149,6 +188,8 @@ def main():
             "successfully bleemed!", "you're all done! happy bleem!castin'.."
         )
     except Exception as e:
+        if debug:
+            print(e)
         messagebox.showerror(
             "error!",
             f"something happened...\n\n{str(e)}\nif you believe this is in error, report it on github.",
